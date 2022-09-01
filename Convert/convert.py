58f86052964c363cb139c6f84221492186946791
AjayKumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2022-09-01 04:43:35 -0400

    Add files via upload

---
+++ Convert/convert.py
@@ -0,0 +1,20 @@
+Integers = [1,2,3,4,5,6,7,8,9]
+
+#List converted into sets
+q = set(Integers)
+print(q)
+
+Real = [1,2,3,4,5,6,7,8,9]
+
+#List converted into Tuple
+p = tuple(Real)
+print(p)
+
+Complex = (1+3j,6+4j)
+
+#Tuple converted into List
+j = list(Complex)
+print(j)
+
+
+
---
+++ Set/setmethods.py
@@ -0,0 +1,17 @@
+Integers = {1,2,3,4,5,6,7}
+Real = {4,7,8.9}
+
+Integers.discard(4)
+print(Integers)
+
+#Intersection
+print(Integers & Real)
+
+#difference
+print(Integers - Real)
+
+#common element
+print(Integers ^ Real)
+
+#Union
+print(Integers | Real)
